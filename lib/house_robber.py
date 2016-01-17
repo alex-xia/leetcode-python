eba0cc0706559db01efa507a4a7eb1b08e2d2c6b
SharkChin <sharkchin@gmail.com>
2016-01-16 23:31:36 -0800

    house_robber use memoization

--- lib/house_robber.py
+++ lib/house_robber.py
@@ -7,6 +7,8 @@ Given a list of non-negative integers representing the amount of money of each h
 '''
 
 class Solution(object):
+    def __init__(self):
+        self.cache = dict()
     def rob(self, nums):
         """
         :type nums: List[int]
@@ -15,15 +17,22 @@ class Solution(object):
         return self.rob_begin(nums, 0)
 
     def rob_begin(self, nums, i):
+        if(i in self.cache):
+            return self.cache[i]
         if i >= len(nums):
             return 0
         if i == len(nums) - 1:
             return nums[i]
         withi = nums[i] + self.rob_begin(nums, i+2)
         withouti = nums[i+1] + self.rob_begin(nums, i+3)
-        return max(withi, withouti)
+        res = max(withi, withouti)
+        self.cache[i] = res
+        return res
 
-if __name__ == 'main':
+if __name__ == '__main__':
     sol = Solution()
     nums = [2,1,1,2]
-    assert sol.rob([nums]) == 4
+    assert sol.rob(nums) == 4
+    sol = Solution()
+    nums = [155,44,52,58,250,225,109,118,211,73,137,96,137,89,174,66,134,26,25,205,239,85,146,73,55,6,122,196,128,50,61,230,94,208,46,243,105,81,157,89,205,78,249,203,238,239,217,212,241,242,157,79,133,66,36,165]
+    assert sol.rob(nums) == 4517
