c242ef6a7e4c8b3cd495b0e9c011b05de57cedba
Alex Xia <alex.xia@mgo.com>
2016-01-19 14:39:30 -0800

    bitwise and range add problem declaration

--- lib/bitwise_and_of_numbers_range.py
+++ lib/bitwise_and_of_numbers_range.py
@@ -1,5 +1,12 @@
 __author__ = 'axia'
 
+'''
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+'''
+
+
 import math
 
 def bformat(num):
