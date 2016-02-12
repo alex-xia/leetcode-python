1fe5f48aa3723215986e0d2df7d3537764f5ff87
Alex Xia <alex.xia@mgo.com>
2016-02-12 11:39:40 -0800

    coin change a shorter solution

--- lib/coin_change.py
+++ lib/coin_change.py
@@ -57,6 +57,26 @@ class Solution(object):
         dfs(coins, amount, 0)
         return -1 if self.min_depth == ubound else self.min_depth
 
+    def coinChange_2(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        coins.sort(reverse = True)
+        lenc, self.res = len(coins), 2**31-1
+
+        def dfs(pt, rem, count):
+            if not rem:
+                self.res = min(self.res, count)
+            for i in range(pt, lenc):
+                if coins[i] <= rem < coins[i] * (self.res-count): # if hope still exists
+                    dfs(i, rem-coins[i], count+1)
+
+        for i in range(lenc):
+            dfs(i, amount, 0)
+        return self.res if self.res < 2**31-1 else -1
+
 
 if __name__ == '__main__':
     sol = Solution()
